c08ac5af28a25cd5ed02bed1ea707c93a4a8a5d9
Khalid Shakeel <khalidshakeel46@gmail.com>
2024-01-05 15:19:44 +0530

    Counter App with useState hook

--- 02CounterApp/src/App.jsx
+++ 02CounterApp/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [value, setValue] = useState(670);
+  const addValue = () => {
+    setValue(value + 10);
+  };
+  const subValue = () => {
+    setValue(value - 10);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Counter App</h1>
+      <h3>Count value is {value}</h3>
+      <button onClick={addValue}>Increment</button>
+      <br />
+      <hr />
+      <button onClick={subValue}>Decrement</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
