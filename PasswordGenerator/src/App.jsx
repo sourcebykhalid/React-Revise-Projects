f76f1b8d13980b321b8b7ee4b51ed62d28180513
Khalid Shakeel <khalidshakeel46@gmail.com>
2024-01-30 12:14:26 +0530

    React Password Generator A simple and customizable password generator built with React. Generate strong and secure passwords with options for length, numbers, and special characters. Copy the generated password to your clipboard with ease.
    Features
    Adjustable password length using a range slider.
    Include or exclude numbers and special characters.
    Copy the generated password to the clipboard.
    Clean and responsive user interface.

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,10 +1,12 @@
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect, useRef } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [number, setNumber] = useState(false);
   const [characters, setCharacters] = useState(false);
   const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
@@ -17,6 +19,11 @@ function App() {
     }
     setPassword(pass);
   }, [length, number, characters, setPassword]);
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 8);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
 
   useEffect(() => {
     return () => {
@@ -26,19 +33,26 @@ function App() {
 
   return (
     <>
-      <div className="bg-orange-400 w-full max-w-lg ml-96 rounded-lg shadow-lg shadow-gray-100 py-4 px-6 mt-36">
+      <h1 className="bg-black text-center text-gray-400 font-bold text-2xl  mt-2 w-full max-w-lg ml-96 py-2 shadow-xl rounded-lg shadow-slate-400 hover:bg-orange-400 cursor-pointer">
+        Password Generator
+      </h1>
+      <div className="bg-orange-400 w-full max-w-lg ml-96 rounded-lg shadow-lg shadow-gray-100 py-4 px-6 mt-12 pb-24 font-bold">
         <h1 className="text-2xl text-slate-950 font-bold px-4 my-3 ">
           Use this Password Generator for Free!
         </h1>
-        <div className="flex mb-4">
+        <div className="flex mb-4 shadow-lg shadow-slate-950 rounded-xl ">
           <input
             type="text"
             value={password}
             readOnly
             placeholder="password"
-            className="outline-none w-full py-1 px-3"
+            className="outline-none w-full py-1 px-3 "
+            ref={passwordRef}
           />
-          <button className="text-md bg-blue-700 px-2 text-slate-50">
+          <button
+            onClick={copyPassword}
+            className="text-md bg-blue-700 px-2 text-slate-50 hover:bg-black"
+          >
             copy
           </button>
         </div>
@@ -46,7 +60,7 @@ function App() {
           <div className="flex items-center gap-x-2">
             <input
               type="range"
-              min={6}
+              min={8}
               max={100}
               value={length}
               className="cursor-pointer"
@@ -71,8 +85,6 @@ function App() {
           <div className="flex items-center gap-x-2">
             <input
               type="checkbox"
-              min={8}
-              max={100}
               value={characters}
               id="char"
               className="cursor-pointer"
@@ -82,6 +94,13 @@ function App() {
             />
             <label htmlFor="char">Characters: {characters}</label>
           </div>
+        </div>
+        <div className="mt-6">
+          <img
+            className="rounded-lg bg-black"
+            src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjkGb2XUwcBCp0c7NB9HIHuG7ZspCtkw1YaDLoI4fdxZxBnvec0vVXBsuBiiQCulxID6dxYBXTs2dbnqgREeXViqoDfrUehCIloexxFEOTpdNvmT0SXQ3zrG6fjbjBZHoT-jGd8t_Z36chtfxZ1wOvmmf9MziUzqwvZzMYTgMYbE47ngR59dPWSunt62uc/s1600/lfralq4h4hu41p6uys3i.jpeg"
+            alt=""
+          />
         </div>
       </div>
     </>
